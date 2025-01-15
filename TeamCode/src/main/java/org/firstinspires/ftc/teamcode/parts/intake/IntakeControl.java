94c66af52ec2d0d92bca8aec1530266cf0fbfb89
Jaswanth <thunderflame2011@gmail.com>
2025-01-14 21:30:29 -0500

    Added color sensor code Changes to tele-op controls Team 14273-FlipBot(Jas and Josh)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/parts/intake/IntakeControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/parts/intake/IntakeControl.java
@@ -4,19 +4,19 @@ public class IntakeControl {
     public int sweepLiftPosition;
     public int sweepSlidePosition;
     public int bucketLiftPosition;
-    public int intakePosition;
+   // public int intakePosition;
     public int pinchPosition;
     public int v_SlidePosition;
     public int intakeAngleSupplier;
 
-    public IntakeControl(int sweeperPower, int sweeperLiftPosition, int sweepSlidePosition, int bucketLiftPosition, int intakePosition,
-        int pinchPosition, int v_SlidePosition, int intakeAngleSupplier){
+    public IntakeControl(int sweeperPower, int sweeperLiftPosition, int sweepSlidePosition, int bucketLiftPosition, //int intakePosition,
+                         int pinchPosition, int v_SlidePosition, int intakeAngleSupplier){
 
         this.sweeperPower = sweeperPower;
         this.sweepLiftPosition = sweeperLiftPosition;
         this.sweepSlidePosition = sweepSlidePosition;
         this.bucketLiftPosition = bucketLiftPosition;
-        this.intakePosition = intakePosition;
+      //  this.intakePosition = intakePosition;
         this.pinchPosition = pinchPosition;
         this.v_SlidePosition = v_SlidePosition;
         this.intakeAngleSupplier = intakeAngleSupplier;
