1b5157685174c412cc2e95e0cbe99c5f9dde5bc5
rhindle <knowlton@gmail.com>
2025-01-26 16:04:30 -0500

    flip - FlipbotSettings tweak

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/parts/intake/FlipbotSettings.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/parts/intake/FlipbotSettings.java
@@ -4,27 +4,21 @@ import om.self.ezftc.utils.Vector3;
 
 public class FlipbotSettings {
 
-   /* the only purpose of this class is to store values accessible to all classes in FlipBot,
-      and which can remain from run to tun
+   /* The purpose of this class is to store values accessible to all classes in FlipBot,
+      and which can remain from run to run
     */
 
-   static boolean isAuto = false;
-   static boolean isTeleOp = false;
+   static boolean modeTeleOp = true;
    static Vector3 robotPosition = new Vector3();   //starts with all zeroes
    public static boolean isRedGood = true;
    public static boolean isYellowGood = true;
    public static boolean isBlueGood = false;
    public static boolean autonomousDebugMode = false;
 
-   public static void setAuto () {
-      isAuto = true;
-      isTeleOp = false;
-   }
-
-   public static void setTeleOp () {
-      isAuto = false;
-      isTeleOp = true;
-   }
+   public static void setTeleOp () { modeTeleOp = true; }
+   public static void setAuto () { modeTeleOp = false; }
+   public boolean isTeleOp() { return modeTeleOp; }
+   public boolean isAuto() { return !modeTeleOp; }
 
    public static void storeRobotPosition(Vector3 currentPosition) {
       if (currentPosition.X == 0 && currentPosition.Y == 0 && currentPosition.Z == 0) return;
