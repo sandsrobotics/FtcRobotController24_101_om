22cbc0cf32f4053e233d7514105f6ee2238c5869
rhindle <knowlton@gmail.com>
2025-01-30 12:59:01 -0500

    servossr continued refinement

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/ServoSSR.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/ServoSSR.java
@@ -15,6 +15,7 @@ public class ServoSSR implements Servo {
     private boolean unknown = true;         // tracks whether the servo position is unknown (due to eStop or external shenanigans)
     private int sweepTime = 1500;           // the time (in ms) it takes the servo to move its entire range (account for loading when setting!)
     private int wakeTime = 200;             // a short interval for the servo to move from disabled/parked to last position
+    private double scale = 1.0;             // scale is potentially useful when replacing a servo with a different range (e.g., 270° vs 300°)
     private long timer = 0;                 // a clock time to track whether a move should be complete
 
     public ServoSSR(Servo servo) {
@@ -29,41 +30,52 @@ public class ServoSSR implements Servo {
 
     /**
      * Sets an offset value for the servo that will be added when setting a position with setPosition()
-     * @param offset the offset to added
+     * @param offset the offset to added, limited to -.25 to +.25.
      * @return this for method chaining
      */
     public ServoSSR setOffset(double offset) {
-        this.offset = offset;
+        this.offset = clamp(offset, -.25, .25);
         return this;
     }
 
     /**
      * Sets the servo sweep time (full time to traverse from minimum to maximum position; e.g., -150° to 150°).
      * The value supplied should account for how the servo is loaded.
-     * @param sweepTime the time in ms
+     * @param sweepTime the time in ms, limited to 0–5000.
      * @return this for method chaining
      */
     public ServoSSR setSweepTime(int sweepTime) {
-        this.sweepTime = sweepTime;
+        this.sweepTime = (int)clamp(sweepTime, 0, 5000);
         return this;
     }
     public ServoSSR setSweepTime(long sweepTime) {
-        this.sweepTime = (int)sweepTime;
-        return this;
+        return setSweepTime((int)sweepTime);
     }
 
     /**
      * Sets the servo "wake" time. If the servo was disabled, this time is allowed for it to return to its previous position.
      * Assumes a small amount of movement has happened while it is disabled.
-     * @param wakeTime the time in ms
+     * @param wakeTime the time in ms, limited to 0–1000.
      * @return this for method chaining
      */
     public ServoSSR setWakeTime(int wakeTime) {
-        this.wakeTime = wakeTime;
+        this.wakeTime = (int)clamp(wakeTime,0,1000);
         return this;
     }
     public ServoSSR setWakeTime(long wakeTime) {
-        this.wakeTime = (int)wakeTime;
+        return setWakeTime((int)wakeTime);
+    }
+
+    /**
+     * Sets a scale value for the servo that will be used (multiplied) when setting a position with setPosition().
+     * The scale will be centered around the midpoint of 0.5.
+     * <P>Note: If scale > 1, the range will be clamped to the range 0–1, and the isAtPosition() and isSetPosition() checks
+     * will not work correctly for the clamped positions.
+     * @param scale the scale multiplier, limited to .5–1.5 (50-150%)
+     * @return this for method chaining
+     */
+    public ServoSSR setScale(double scale) {
+        this.scale = clamp(scale, 0.5, 1.5);
         return this;
     }
 
@@ -82,9 +94,32 @@ public class ServoSSR implements Servo {
      * @return this for method chaining
      */
     public ServoSSR setPwmRange(double low, double high) {
-        // could use some checking
-        ((ServoImplEx)servo).setPwmRange(new PwmControl.PwmRange(low, high));
+        double lowC = clamp(low,500,2500);
+        double highC = clamp(high,500,2500);
+        if (lowC > highC) {
+            double temp = lowC;
+            lowC = highC;
+            highC = temp;
+        }
+        ((ServoImplEx)servo).setPwmRange(new PwmControl.PwmRange(lowC, highC));
         return this;
+    }
+
+    /**
+     * Resets the internal movement timer. Potentially useful when you know the tracking timer may not be accurate (e.g., during initialization
+     * or when the servo is under heavy load). With no parameter, defaults to the full sweep time.
+     * <p>Note that when setting a new position, the delay will be reset with a maximum of the full sweep time, so if you want to delay isDone()
+     * longer than that, you will need to use this method after setting the position.
+     * @param delay the amount of time in ms to set the timer in the future (one time only). If not provided, defaults to sweep time.
+     *              Limited to 0–5000.
+     * @return this for method chaining
+     */
+    public ServoSSR resetTime(int delay) {
+        timer = System.currentTimeMillis() + (long)clamp(delay,0,5000);
+        return this;
+    }
+    public ServoSSR resetTime() {
+        return resetTime(sweepTime);
     }
 
     // enabling and disabling pwm
@@ -141,7 +176,9 @@ public class ServoSSR implements Servo {
      * @return the servo position - offset
      */
     public double getPositionWithOffset() {
-        return clamp(getPosition() - offset);
+        //return clamp(getPosition() - offset);
+        // reverse order... subtract the offset, then inversely scale
+        return unscalePos(clamp(getPosition() - offset));
     }
 
     /**
@@ -188,6 +225,10 @@ public class ServoSSR implements Servo {
      */
     public boolean isSetPosition(double comparePosition) {
         return(Math.round(getPositionWithOffset()*100.0) == Math.round(comparePosition*100.0));  // deals with rounding error
+    }
+    private boolean isSetPos(double comparePosition) {
+        // for internal use, where the position is already scaled and offset
+        return(Math.round(getPosition()*100.0) == Math.round(comparePosition*100.0));  // deals with rounding error
     }
 
     /**
@@ -299,12 +340,17 @@ public class ServoSSR implements Servo {
 
     @Override
     public void setPosition(double position) {
+        /* 0. Scale and offset the position */
+        double pos = clamp(scalePos(position) + offset);
+
         /* 1. Make sure the state variables are up to date */
         isEnabled();
 
         /* 2. Don't update the timer if enabled and the position is already set the same */
-        if (enabled && isSetPosition(position)) {
-            servo.setPosition(clamp(position + offset));          // this probably isn't needed, but added while debugging undesirable behavior
+        //if (enabled && isSetPosition(position)) {
+        if (enabled && isSetPos(pos)) {
+            //servo.setPosition(clamp(position + offset));          // this probably isn't needed, but added while debugging undesirable behavior
+            servo.setPosition(pos);          // this probably isn't needed, but added while debugging undesirable behavior
             return;                                        // has already been set (but not necessarily done moving), no need to increment timer
         }
 
@@ -313,8 +359,10 @@ public class ServoSSR implements Servo {
         if (!enabled) enable();
 
         /* 4. Calculate the timer and set the position */
-        timer = calcSweepTimerValue(position);
-        servo.setPosition(clamp(position + offset));
+        //timer = calcSweepTimerValue(position);
+        timer = calcSweepTimerValue(pos);
+        //servo.setPosition(clamp(position + offset));
+        servo.setPosition(pos);
 
         /* 5. Update tracking variables */
         enabled = true;                                    // setting a position re-enables, so update the trackers
@@ -387,11 +435,23 @@ public class ServoSSR implements Servo {
     }
 
     private double calcSweepChange(double newPosition) {
-        return Math.abs(getPositionWithOffset()-newPosition);
+        //return Math.abs(getPositionWithOffset()-newPosition);
+        return Math.abs(getPosition()-newPosition);
     }
 
     private double clamp(double pos) {
         return Math.max(0, Math.min(pos, 1));
+    }
+    private double clamp(double val, double min, double max) {
+        return Math.max(min, Math.min(val, max));
+    }
+
+    private double scalePos(double pos) {
+        return clamp(((clamp(pos) - 0.5) * scale) + 0.5);
+    }
+
+    private double unscalePos(double pos) {
+        return clamp(((clamp(pos) - 0.5) / scale) + 0.5);
     }
 }
 
@@ -411,10 +471,12 @@ All of the ordinary Servo methods and properties are available with the followin
 
 setPosition() - sets the position with offset and calculates the expected movement time
 setOffset(offset) - set an offset that will be added to positions (for tuning a replacement servo or one of a pair acting together)
+setScale(scale) - set a scale multiplier for positions
 setSweepTime(sweepTime) - set the time expected for the servo to move its entire range
 setWakeTime(wakeTime) - set the time expected for the servo to move back to its position after being disabled
 setFullPwmRange() - sets the controller to use pwm range of 500-2500 μs vs. the default of 600-2400 μs
 setPwmRange(low, high) - sets the controller to use an arbitrary pwm range
+resetTime(delay) - resets the internal timer to delay or default of sweepTime
 stop() - disables the servo pwm signal and its position will be unknown/unpredictable (e.g., emergency stop)
 disable() - disables the servo pwm signal and assumes it will stay near its last position (e.g., docked or parked)
 enable() - enables the servo pwm signal (not usually necessary to do manually)
@@ -429,5 +491,8 @@ isEnabled() - is the servo enabled?
 isDisabled() - is the servo disabled?
 isStopped() - is the servo stopped?
 getServo() - get the servo object for whatever reason
+setPower(power) - sets the "power" like a CRservo, -1 to 1.
+getPower() - returns the "power", -1 to 1.
+setBlinkinPattern(pattern) - sets using pattern as int or RevBlinkinLedDriver.BlinkinPattern; must use setFullPwmRange()
 
  */
