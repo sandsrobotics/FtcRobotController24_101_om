e86bc75a1a9b6738d025dc610d50d7a72bdbf010
Tom Kroon <soccer@kroon.ws>
2025-01-31 22:10:25 -0500

    Friday night Harsh

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/parts/intake2/settings/IntakeTeleopSettings2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/parts/intake2/settings/IntakeTeleopSettings2.java
@@ -15,14 +15,15 @@ public class IntakeTeleopSettings2 {
     public final Supplier<Float> strafeSpeedSupplier;
     public final Supplier<Boolean> autoHomeSupplier;
     public final Supplier<Integer> specimenServoSupplier;
-    public final Supplier<Integer> robotLiftToZeroSupplier;
-    public final Supplier<Integer> robotEStopSupplier;
+    public final Supplier<Boolean> robotEStopSupplier;
+    public final Supplier<Boolean> rangeingSupplier;
 
     public IntakeTeleopSettings2(Supplier<Double> sweepSpeedSupplier, Supplier<Integer> sweepLiftSupplier,
                                  Supplier<Integer> sweepSlideSupplier, Supplier<Integer> bucketLiftSupplier,
                                  Supplier<Integer> robotLiftSupplier,Supplier<Integer> rotationServoSupplier,
                                  Supplier<Float> strafeSpeedSupplier, Supplier<Boolean> autoHomeSupplier,
-                                 Supplier<Integer> specimenServoSupplier, Supplier<Integer> robotLiftToZeroSupplier, Supplier<Integer> robotEStopSupplier) {
+                                 Supplier<Integer> specimenServoSupplier, Supplier<Boolean> robotEStopSupplier,
+                                 Supplier<Boolean> rangeingSupplier) {
         this.sweepSpeedSupplier = sweepSpeedSupplier;
         this.sweepLiftSupplier = sweepLiftSupplier;
         this.sweepSlideSupplier = sweepSlideSupplier;
@@ -32,8 +33,8 @@ public class IntakeTeleopSettings2 {
         this.strafeSpeedSupplier = strafeSpeedSupplier;
         this.autoHomeSupplier = autoHomeSupplier;
         this.specimenServoSupplier = specimenServoSupplier;
-        this.robotLiftToZeroSupplier = robotLiftToZeroSupplier;
         this.robotEStopSupplier = robotEStopSupplier;
+        this.rangeingSupplier = rangeingSupplier;
     }
 
     public static IntakeTeleopSettings2 makeDefault(Robot robot) {
@@ -45,13 +46,13 @@ public class IntakeTeleopSettings2 {
                 () -> gamepad.y ? -1 : gamepad.a ? 1 : 0, // sweepLiftSupplier
                 () -> gamepad.x ? -1 : gamepad.b ? 1 : 0, // horizontal SlideSupplier -1 = in
                 () -> gamepad2.y ? 1 : gamepad2.a ? -1 : 0, // bucketLiftSupplier
-                () -> gamepad2.left_bumper ? -1 :gamepad2.right_bumper ? 1 : 0, // robotLiftSupplier
+                () -> gamepad2.left_bumper ? -1 : gamepad2.right_bumper ? 1 : 0, // robotLiftSupplier
                 () -> gamepad.dpad_left ? -1 : gamepad.dpad_right ? 1 : 0, // rotationServoSupplier
                 () -> gamepad2.left_stick_x, // strafeSpeedSupplier
                 new EdgeSupplier(() -> gamepad.back).getRisingEdgeSupplier(), // autoHomeSupplier
                 () -> gamepad2.dpad_left ? 1 : gamepad2.dpad_up ? 2 : gamepad2.dpad_right ? -1 : 0, // specimenServoSupplier
-                () -> gamepad2.right_bumper ? 1 : 0, // robotLiftToZeroSupplier
-                () -> gamepad.start ? 1 : 0 // robotEStopSupplier
+                () -> gamepad.right_stick_button || gamepad2.right_stick_button, // robotEStopSupplier
+                () -> gamepad2.b // range on off
         );
     }
 }
