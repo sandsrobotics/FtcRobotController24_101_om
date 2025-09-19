8a731da04e052a2eb9bbb94bd6e05f1bdf89da97
rhindle <knowlton@gmail.com>
2025-09-19 19:53:36 -0400

    New flipbot opmode for demo that allows intake of any color

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FlipTeleopForzaAnyColor.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.parts.intake.FlipbotSettings;
+
+@TeleOp(name="14273.2 Forza ANY COLOR", group="14273")
+public class FlipTeleopForzaAnyColor extends FlipTeleopForza {
+    @Override
+    public void extraSettings() {
+        FlipbotSettings.isBlueGood = true;
+        FlipbotSettings.isYellowGood = true;
+        FlipbotSettings.isRedGood = true;
+    }
+}
